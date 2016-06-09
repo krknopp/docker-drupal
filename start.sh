66b0ec8014e63406160910f3c525ff689763ff40
Kerry Knopp <kerry@codekoalas.com>
2016-06-09 17:54:15 -0500

    add Docker Console and logging

--- start.sh
+++ start.sh
@@ -8,3 +8,7 @@ git clone -b $GIT_BRANCH $GIT_REPO /var/www/site/
 
 # Symlink Settings file if not already done
 cd /var/www/site/docroot/sites/default && ln -s remote.settings.php settings.php
+
+# Symlink files folder
+cd /var/www/site/docroot/sites/default && ln -sfn /mnt/sites-files/public files
+cd /var/www/site/ && ln -sfn /mnt/sites-files/private private
