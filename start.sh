a250b2e91897de26a757219c081400096f970649
Kerry Knopp <kerry@codekoalas.com>
2016-06-23 13:32:20 -0500

    changing drupal version detection method

--- start.sh
+++ start.sh
@@ -10,11 +10,11 @@ grep -q -F "$GIT_HOSTS" /etc/hosts  || echo $GIT_HOSTS >> /etc/hosts
 git clone -b $GIT_BRANCH $GIT_REPO /var/www/site/
 
 # Symlink Settings file if not already done
-cd /var/www/site/docroot/sites/default && ln -s remote.settings.php settings.php
+#cd /var/www/site/docroot/sites/default && ln -s remote.settings.php settings.php
 
 # Symlink files folder
 cd /var/www/site/docroot/sites/default && ln -sfn /mnt/sites-files/public files
 cd /var/www/site/ && ln -sfn /mnt/sites-files/private private
 
 # Set DRUPAL_VERSION
-export DRUPAL_VERSION=$(/usr/local/src/drush/drush --root=$APACHE_DOCROOT status | grep "Drupal version" | awk '{ print substr ($(NF), 0, 2) }')
+echo $(/usr/local/src/drush/drush --root=$APACHE_DOCROOT status | grep "Drupal version" | awk '{ print substr ($(NF), 0, 2) }') > /root/drupal-version.txt
